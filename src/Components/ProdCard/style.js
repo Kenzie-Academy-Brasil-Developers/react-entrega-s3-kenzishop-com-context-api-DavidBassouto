e3f1e2d63c3efc8de0e35d5cba91ceebc0c82a3d
DavidBassouto <davidbassouto@hotmail.com>
2022-06-29 14:49:56 -0300

    fix:ajuste no tamanho do conbtainer

--- src/Components/ProdCard/style.js
+++ src/Components/ProdCard/style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  max-height: 450px;
+  max-height: 420px;
   display: flex;
   flex-direction: column;
   align-items: center;
